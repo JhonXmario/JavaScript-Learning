bf284e229baee886c66dd8e612205a18e9301aca
JhonXmario <149451835+JhonXmario@users.noreply.github.com>
2025-03-26 16:48:13 -0400

    Update 04-Strings.js

--- 04-Strings.js
+++ 04-Strings.js
@@ -19,10 +19,21 @@ console.log(greeting.indexOf("Jhon")); // Esto te muesta el indice de en donde e
 console.log(greeting.indexOf("XYZ"));
 
 console.log(greeting.includes("Jhon"));
-console.log(greeting.includes("Hola"));
+console.log(greeting.includes("Hola")); // Esto te dice si el String contiene la palabra especificada
 console.log(greeting.includes("XYZ"));
 
 console.log(greeting.slice(0, 7)) // Te imprime los caracteres del String desde hasta donde especifiques
 console.log(greeting.replace('Jhon', "Mario")) // Reemplaza un valor por el valor especificado luego de la coma
 
-// continuare luego, tengo que dormir =|
+// Template Literals (plantillas literales)
+let message = `Hola 
+buenos
+ dias
+Ahora 
+si 
+sirve 
+jejejeje`
+
+console.log(message)
+let alias = "MarioXYZ"
+console.log(`Hola, ${myname}! Tu usuario es ${alias}`); // Esto es una forma de llamar a una variable dentro de un String, usando los ``, el nombre de la variable entre llaves y '$' al lado
