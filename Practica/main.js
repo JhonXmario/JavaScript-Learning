e19178bf2010694b8363eb197591552f516ef473
JhonXmario <149451835+JhonXmario@users.noreply.github.com>
2025-04-03 17:01:19 -0400

    Delete Practica directory

--- Practica/main.js
+++
@@ -1,2 +0,0 @@
-const { adress, number, array, user } = require("./modules/my-module");
-console.log(adress, number, array, user);
--- Practica/timers.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-    console.log("object");
-}, 2000);
-
-setTimeout(() => {
-    console.log("object2");
-}, 5000);
