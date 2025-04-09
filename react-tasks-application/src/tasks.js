44c0d6e02bd1b5d289f65a5769c0144e94fda4d7
JhonXmario <149451835+JhonXmario@users.noreply.github.com>
2025-04-09 16:51:41 -0400

    Add files via upload

---
+++ react-tasks-application/src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    descripcion: "lo mismo que arriba",
+  },
+  {
+    id: 1,
+    title: "mi Segunda tarea",
+    descripcion: "lo mismo que arriba",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    descripcion: "lo mismo que arriba",
+  }
+];
